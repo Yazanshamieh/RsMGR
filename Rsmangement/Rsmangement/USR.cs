d15a804f23e674485b5a3dbae6894e79f650a603
Yazanshamieh <yazanshamieh62@gmail.com>
2024-12-03 21:35:05 +0200

    Add files via upload

---
+++ Rsmangement/Rsmangement/USR.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rsmangement
+{
+	public partial class USR : Form
+	{
+		public USR()
+		{
+			InitializeComponent();
+		}
+
+		private void USR_Load(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
